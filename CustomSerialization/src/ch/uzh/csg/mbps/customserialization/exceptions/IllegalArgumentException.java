6460602dbb7b4f9f6942f24be2b2cabf0fa859b2
Jeton Memeti <jeton.memeti@gmail.com>
2014-06-05 10:34:59 +0200

    Javadoc

--- CustomSerialization/src/ch/uzh/csg/mbps/customserialization/exceptions/IllegalArgumentException.java
+++ CustomSerialization/src/ch/uzh/csg/mbps/customserialization/exceptions/IllegalArgumentException.java
@@ -2,7 +2,7 @@ package ch.uzh.csg.mbps.customserialization.exceptions;
 
 /**
  * In contrast to {@link java.lang.IllegalArgumentException} this extends
- * {@link Exception} and is therefore caught. Therefore, it has explicitly to be
+ * {@link Exception} and is therefore checked. Therefore, it has explicitly to be
  * caught when calling methods which might throw it.
  * 
  * @author Jeton Memeti
