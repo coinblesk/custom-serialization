d4cf69f69bc3bb7272b75a5ff60dd7ef04c3e940
Jeton Memeti <jeton.memeti@gmail.com>
2014-08-26 16:18:19 +0200

    Fixed PaymentRequest

--- CustomSerialization/src/main/java/ch/uzh/csg/mbps/customserialization/PaymentRequest.java
+++ CustomSerialization/src/main/java/ch/uzh/csg/mbps/customserialization/PaymentRequest.java
@@ -150,11 +150,11 @@ public class PaymentRequest extends SignedSerializableObject {
 		if (currency == null)
 			throw new IllegalArgumentException("The currency cannot be null.");
 		
-		if (amount <= 0)
-			throw new IllegalArgumentException("The amount must be greatern than 0.");
+		if (amount < 0)
+			throw new IllegalArgumentException("The amount must be greater than 0.");
 		
 		if (timestamp <= 0)
-			throw new IllegalArgumentException("The timestamp must be greatern than 0.");
+			throw new IllegalArgumentException("The timestamp must be greater than 0.");
 	}
 	
 	private void setPayload(boolean hasInputCurrency) {
